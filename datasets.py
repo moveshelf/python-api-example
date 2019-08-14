ad8ab0e48ce5615af67dfcb41cee476c53eb1d71
Ignazio Aleo <ignazio.aleo@moveshelf.com>
2019-08-15 00:04:06 +0200

    Moving project id argument for clarity

--- datasets.py
+++ datasets.py
@@ -4,11 +4,12 @@ from io import BytesIO
 
 
 class Dataset(object):
-    def __init__(self, mvshlf_api):
+    def __init__(self, mvshlf_api, project_id):
         self.mvshlf_api = mvshlf_api
+        self.project_id = project_id
 
-    def load_data(self, project_id):
-        dataset = self.mvshlf_api.getProjectDatasets(project_id)
+    def load_data(self):
+        dataset = self.mvshlf_api.getProjectDatasets(self.project_id)
         url = dataset[0]['downloadUri']
         r = requests.get(url, stream = True)
         self.data = np.load(BytesIO(r.raw.read()))
